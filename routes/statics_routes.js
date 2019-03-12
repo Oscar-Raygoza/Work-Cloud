4757cbe759621237b0665d36657107f5be30689d
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-03-12 16:20:30 -0600

    adding new routes statics

--- routes/statics_routes.js
+++ routes/statics_routes.js
@@ -5,6 +5,8 @@ let StaticsController = require('../controllers/statics');
 let router = express.Router();
 
 router.route('/').get(StaticsController.index);
-
+router.route('/help').get(StaticsController.help);
+router.route('/404').get(StaticsController.error404);
+router.route('/register').get(StaticsController.register);
 
 module.exports = router;
