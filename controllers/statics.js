1beeb12655bafcbb43a8aaa0df4e4e0e5f4c00cf
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-03-12 16:18:39 -0600

    adding new routes

--- controllers/statics.js
+++ controllers/statics.js
@@ -1,5 +1,18 @@
 module.exports = {
     index: function(req,res){
-            res.render('statics/index');
+        res.render('statics/index');
+    },
+    error404: function(req,res){
+        res.render('statics/404-error');
+    },
+    help: function(req,res){
+        res.render('statics/help');
+    },
+    register: function(req,res){
+        res.render('statics/register');
+    },
+    login: function(res,req){
+        res.render('statics/login');
     }
+
 }
