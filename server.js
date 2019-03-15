3b900ae012aa360571232ecc98e74fad0cb9388c
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-03-15 00:36:30 -0600

    configuring sqlite3

--- server.js
+++ server.js
@@ -8,20 +8,113 @@ const compression = require('compression');
 const app = express();
 
 const sqlite3 = require('sqlite3');
+/*
+const sequelize = new Sequelize('work_cloud_db',null,null,{
+    dialect: 'sqlite',
+    storage: './work_cloud_db'
+})
+ */
 
-let db = new sqlite3.Database('work_cloud_db');
+ let db = new sqlite3.Database("work_cloud_db")
 
-db.run(`CREATE TABLE users(
-        id_user INT PRIMARY KEY,
-        name VARCHAR(50),
-        lastname VARCHAR(60),
-        email VARCHAR(60) UNIQUE,
-        telephone VARCHAR(15) UNIQUE,
-        sex CHAR,
-        password_hash  VARCHAR(36)
-        )`);
-db.close();
 
+ db.run(`
+    CREATE TABLE user(
+      id_user INT PRIMARY KEY,
+      name VARCHAR(40) NOT NULL,
+      lastname VARCHAR(50) NOT NULL,
+      nickname VARCHAR(20) UNIQUE NOT NULL,
+      sex VARCHAR(2) NOT NULL,
+      email VARCHAR(50) UNIQUE NOT NULL,
+      number VARCHAR(15) UNIQUE NOT NULL,
+      password_hash VARCHAR(250) NOT NULL
+    )
+
+    CREATE TABLE profile_picture(
+      id_profile_picture INT PRIMARY KEY,
+      id_user INT NOT NULL,
+      profile_picture BLOB,
+      likes INT,
+      FOREIGN KEY(id_user) REFERENCES user(id_user)
+    )
+
+    CREATE TABLE profile_banner(
+      id_profile_picture INT PRIMARY KEY,
+      id_user INT NOT NULL,
+      profile_banner BLOB,
+      likes INT,
+      FOREIGN KEY(id_user) REFERENCES user(id_user)
+    )
+    
+    CREATE TABLE comment(
+      id_comment INT PRIMARY KEY,
+      id_user INT,
+      likes INT,
+      comment VARCHAR(255),
+      FOREIGN KEY(id_user) REFERENCES user(id_user)
+    )
+
+    CREATE TABLE publication_post(
+      id_publication_post INT PRIMARY KEY,
+      id_user INT,
+      post VARCHAR(255) NOT NULL,
+      FOREIGN KEY(id_user) REFERENCES user(id_user)
+    )
+
+    CREATE TABLE publication_image(
+      id_publication_image INT PRIMARY KEY,
+      id_user INT,
+      description  VARCHAR(255),
+      post_img BLOB,
+      FOREIGN KEY(id_user) REFERENCES user(id_user)
+    )
+
+    CREATE TABLE publication_video(
+      id_publication_video INT PRIMARY KEY,
+      id_user INT,
+      description  VARCHAR(255),
+      post_video BLOB,
+      FOREIGN KEY(id_user) REFERENCES user(id_user)
+    )
+
+    CREATE TABLE publication_pdf(
+      id_publication_video INT PRIMARY KEY,
+      id_user INT,
+      description  VARCHAR(255),
+      post_pdf BLOB,
+      FOREIGN KEY(id_user) REFERENCES user(id_user)
+    )
+    
+    CREATE TABLE publication_code(
+      id_publication_video INT PRIMARY KEY,
+      id_user INT,
+      code VARCHAR,
+      FOREIGN KEY(id_user) REFERENCES user(id_user)
+    )
+
+    CREATE TABLE comments_publication(
+      id_comment INT,
+      id_publication INT, 
+      type VARCHAR(10),
+      FOREIGN KEY(id_comment) REFERENCES comment(id_comment)
+    )
+
+    CREATE TABLE grup(
+      id_grup INT PRIMARY KEY,
+      name VARCHAR(50),
+      category VARCHAR(50),
+    )
+    
+    CREATE TABLE grup_user(
+      id_user INT,
+      id_grup INT,
+      FOREIGN KEY(id_user) REFERENCES user(id_user),
+      FOREIGN KEY(id_grup) REFERENCES grup(id_grup)
+    )
+    
+ `,(err)=>{
+   console.log(err);
+ });
   /**Confugurando las cookis */
 app.use(cookisSession({
     name: 'galleta',
