fa5eb115991351c57a1210b0bde5cd6cf45ab11d
Oscar Raygoza <oscar.eduardo.raygoza@gmail.com>
2019-03-17 18:48:33 -0600

    add new route login

--- routes/statics_routes.js
+++ routes/statics_routes.js
@@ -8,5 +8,7 @@ router.route('/').get(StaticsController.index);
 router.route('/help').get(StaticsController.help);
 router.route('/404').get(StaticsController.error404);
 router.route('/register').get(StaticsController.register);
+router.route('/login').get(StaticsController.login);
+
 
 module.exports = router;
