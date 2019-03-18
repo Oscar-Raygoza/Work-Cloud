a606f58be04afb1a26524b0db3a13060f9265cc5
Oscar Raygoza <oscar.eduardo.raygoza@gmail.com>
2019-03-17 18:48:12 -0600

    adding model and migration TABLE user

--- models/user.js
+++ models/user.js
@@ -1,7 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    id_user: DataTypes.TEXT
+    email: DataTypes.TEXT,
+    name:  DataTypes.TEXT,
+    lastname:  DataTypes.TEXT,
+    number: DataTypes.TEXT,
+    nickname: DataTypes.TEXT,
+    password_hash: DataTypes.TEXT
   }, {});
   User.associate = function(models) {
     // associations can be defined here
