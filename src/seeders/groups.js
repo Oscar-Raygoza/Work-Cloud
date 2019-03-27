e00daf2c37844bd49a72b9b9f418e932221f5cb6
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-03-26 21:24:19 -0600

    public files

---
+++ src/seeders/groups.js
@@ -0,0 +1,32 @@
+db.groups.insert({
+    "name": "Facebook React",
+    "filenameIcon": "oe3osa3.png",
+    "description": "This group is to compare doubts about the react framework",
+    "timestanp" : ISODate("2019-03-25T05:42:08.998Z"),
+    "members": 0
+})
+
+db.groups.insert({
+    "name": "Angular 4",
+    "filenameIcon": "dsa2e3e.png",
+    "description": "This group is to compare doubts about the Angular framework",
+    "timestanp" : ISODate("2018-03-25T05:42:08.998Z"),
+    "members": 500
+})
+
+db.groups.insert({
+    "name": "Bootstrap 4",
+    "filenameIcon": "ee3esa3.png",
+    "description": "This group is to compare doubts about the Bootstrap 4 framework",
+    "timestanp" : ISODate("2019-02-25T05:42:08.998Z"),
+    "members": 52
+})
+
+db.groups.insert({
+    "name": "MongoDB",
+    "filenameIcon": "ae6evab.png",
+    "description": "This group is to compare doubts about the MongoDB",
+    "timestanp" : ISODate("2019-02-15T05:42:08.998Z"),
+    "members": 520
+})
+
