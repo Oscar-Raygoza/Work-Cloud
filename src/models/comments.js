1a86aeba4f417e3af8c270975455a047e106787b
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-03-26 21:28:36 -0600

    model publications

---
+++ src/models/comments.js
@@ -0,0 +1,16 @@
+const { Schema } = require("mongoose");
+const { ObjectId } = Schema;
+
+const CommentSchema = new Schema({
+    title: { type: String },
+    description: { type: String },
+    filename: { type: String },
+    views: { type: Number, default: 0 },
+    likes: { type: Number, default: 0 },
+    timestanp: { type: Date, default: Date.now }
+});
+ImageSchema.virtual('uniqueId')
+    .get(function(){
+        return this.filename.replace(path.extname(this.filename),'');
+    });
+module.exports = mongoose.model('Image', ImageSchema)
