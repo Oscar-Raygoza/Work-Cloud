64f9e517d232e519f271bcba7a660cdde1eb89b0
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-03-29 00:03:21 -0600

    update home.js

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,9 +1,16 @@
 const ctrl = {};
 const { Publications } = require("../models/index")
+const slidebar= require('../helpers/slidebar')
 
 ctrl.index = async (req, res) =>{
-    const postPublications = await Publications.find().sort({timestanp: -1}).limit(3);
-    res.render('index', { postPublications })
+    const postPublications = await Publications.find().sort({timestanp: -1});
+    let viewModel = {
+        publication: []
+    }
+    viewModel.publication = postPublications;
+    viewModel = await slidebar(viewModel); 
+    console.log(viewModel)
+    res.render('index',  viewModel )
 }
 
 
