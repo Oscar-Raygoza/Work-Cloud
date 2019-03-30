6fe3da678797f8657e9f11dd91d7eaea52e80af2
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-03-30 00:22:05 -0600

    adding new deoendencies

--- src/server/config.js
+++ src/server/config.js
@@ -6,7 +6,17 @@ const multer = require('multer');
 const express = require('express');
 const errorhandler = require('errorhandler');
 
-const routes = require('../routes/index')
+const routes = require('../routes/index');
+const routesSession = require('../routes/sessions');
+
+/** Login */
+const flash = require('connect-flash');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+require('./passport',(passport))
+
 module.exports = (app) =>{
 
       //Settings 
@@ -26,9 +36,19 @@ module.exports = (app) =>{
       app.use(multer({dest: path.join(__dirname, '../public/upload/temp')}).single('publication'))
       app.use(express.urlencoded({extended: false}));
       app.use(express.json());
-    // routes
-      routes(app);
+      app.use(cookieParser());
+      app.use(bodyParser.urlencoded({extended: false}));
+      app.use(session({
+        secret: 'xcsda3Raygoza3e6fda3',
+        resave: false,
+        saveUninitialized: false
+      }));
+      app.use(passport.initialize());
+      app.use(passport.session());
 
+    // routes
+    routes(app);
+    routesSession(app, passport);
     // static files
     app.use('/public', express.static(path.join(__dirname, '../public')));
     
