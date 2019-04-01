159d1b20ef6535b51e59668031c89e6b9c241b67
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-04-01 01:04:22 -0600

    new routes in sessions

--- src/routes/sessions.js
+++ src/routes/sessions.js
@@ -1,9 +1,34 @@
-const express = require('express');
-const router = express.Router();
-
 const Sessions = require('../controllers/sessions');
+const passport = require('passport');
 
-module.exports = (app, passport) =>{
-    app.get('/login', Sessions.index)
+module.exports = (app) =>{
+    app.get('/login', Sessions.index);
+    app.get('/signup', Sessions.signup);       
+    app.get('/signin' , Sessions.signin);  
+    app.get('/profile' , isAuthenticated,Sessions.profile);  
+
+    app.post('/signup',  passport.authenticate('local-signup', {
+        successRedirect: '/profile',
+        failureRedirect: '/signup',
+        passReqToCallback: true
+    }));
+
+    app.post('/signin',  passport.authenticate('local-signin', {
+        successRedirect: '/profile',
+        failureRedirect: '/signin',
+        passReqToCallback: true
+    }));
+
+    app.get('/logout', (req,res,next)=>{
+        req.logout();
+        console.log(req.logout());
+        res.redirect('/');
+     });
     
+    function isAuthenticated(req, res, next){
+        if(req.isAuthenticated){
+          return next();
+        }
+        res.redirect('/')
+    }
 };
