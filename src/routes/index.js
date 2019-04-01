a9f412665b897a56f5913e03e8a8cdf223b0a8bd
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-04-01 15:04:22 -0600

    add passport

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
 const home = require('../controllers/home');
 const publications = require('../controllers/publications');
 
