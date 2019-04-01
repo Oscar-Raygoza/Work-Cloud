7888505b043599530f26857d0642c209b1e8f93d
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-04-01 01:02:05 -0600

    implement sessions bug

--- src/controllers/sessions.js
+++ src/controllers/sessions.js
@@ -1,10 +1,35 @@
 const ctrl = {};
 const { Publications } = require("../models/index")
+const Passport = require('passport');
+
 
 ctrl.index = async (req, res) =>{
    res.render('login');
-   
 }
+
+ctrl.signup = async (req, res, next) =>{
+   res.render('signup');
+}
+
+
+ctrl.signin = async (req, res, next) =>{
+   res.render('signin');
+}
+
+
+ctrl.profile = async (req, res, next) =>{
+   res.send('fs');
+}
+
+ctrl.notprofile = async (req, res, next) =>{
+   res.send('FALSE')
+}
+
+ctrl.logout =(req,res,next)=>{
+   req.logout();
+   console.log(req.logout());
+   res.redirect('/');
+};
 
 
 module.exports = ctrl;
