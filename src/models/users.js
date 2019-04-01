f1c69a26bad14b3f7a0f4b4522bec671f406b13d
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-04-01 01:02:33 -0600

    new model users

---
+++ src/models/users.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const bcrypt = require('bcrypt-nodejs');
+
+
+const UsersSchema = new Schema({
+    name: { type: String },
+    lastname: { type: String },
+    email: { type: String },
+    number: { type: String },
+    sex: { type: String },
+    image_profile: { type: String },
+    nickname: {type: String },
+    password: { type: String },
+    timestamp: { type: Date, default: Date.now }
+}); 
+
+UsersSchema.methods.encryptPassword = (password)=>{
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+};
+UsersSchema.methods.unencryptPassword = (password)=>{
+    return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('users', UsersSchema)
