2055dd297d409bbc6b5404594f19716d12f26d35
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-04-01 01:05:20 -0600

    add new middlewares

--- src/server/config.js
+++ src/server/config.js
@@ -15,11 +15,12 @@ const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-require('./passport',(passport))
 
 module.exports = (app) =>{
 
       //Settings 
+      require('../passport/local-auth')
+
       app.set('port', process.env.PORT || 3000)
       app.set('views' , path.join(__dirname, '../views'))
       app.engine('.hbs', exphbs({
@@ -38,6 +39,7 @@ module.exports = (app) =>{
       app.use(express.json());
       app.use(cookieParser());
       app.use(bodyParser.urlencoded({extended: false}));
+      app.use(flash());
       app.use(session({
         secret: 'xcsda3Raygoza3e6fda3',
         resave: false,
@@ -46,8 +48,13 @@ module.exports = (app) =>{
       app.use(passport.initialize());
       app.use(passport.session());
 
+      app.use((req,res,next) => {
+        app.locals.signupMessage = req.flash('signupMessage');
+        next();
+      });
+
     // routes
-    routes(app);
+    routes(app,passport);
     routesSession(app, passport);
     // static files
     app.use('/public', express.static(path.join(__dirname, '../public')));
