937fbb8abc7cb22c3e3123f04f22003b8c94955a
Oscar Raygoza <oscar.eduardo.raygoza@gmail.com>
2019-04-01 18:06:58 -0600

    add js function scroll

---
+++ src/public/assets/js/front-index.js
@@ -0,0 +1,34 @@
+window.onscroll = function() { scrollMenuFixed() };
+
+scrollMenuFixed();
+function scrollMenuFixed() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) 
+    document.getElementById("menu-fixed").className = "menu-fixed blur";
+         else 
+    document.getElementById("menu-fixed").className = "menu-fixed";
+
+    /*
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) 
+    document.getElementById("banner-img-index").className = "banner-img animateScrollMove";
+         else 
+    document.getElementById("banner-img-index").className = "banner-img";
+
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) 
+    document.getElementById("banner-img-index-down").className = "banner-img animateScrollMove";
+         else 
+    document.getElementById("banner-img-index-down").className = "banner-img";
+     */
+}
+
+
+var toggleMenu = document.getElementById("toggle-menu");
+
+        toggleMenu.addEventListener('click',()=>{
+          toggleMenu.classList.toggle('nav-is-visible');
+});
+
+
+
+
+
+
