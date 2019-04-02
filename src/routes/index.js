8cbc46b6a1d14a7b4eb8518e4a4ad72a0dd29051
Oscar Raygoza <oscar.eduardo.raygoza@gmail.com>
2019-04-01 18:07:48 -0600

    add function authenticated

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,20 @@ const home = require('../controllers/home');
 const publications = require('../controllers/publications');
 
 module.exports = app =>{
-router.get('/', home.index);
+router.get('/', isAuthenticated,home.index);
 router.get('/publications/:publication_id', publications.index);
 router.post('/publications/', publications.create);
 router.post('/publications/:publications_id/like', publications.like);
 router.post('/publications/:publications_id/comment', publications.comment);
 router.delete('/publications/:publications_id', publications.delete);
+router.get('/home', home.home)
+
+function isAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+      return next();
+    }
+    res.redirect('/home')
+}
 
 app.use(router);
 };
