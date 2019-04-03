7249ef6dee749a10b053f260ab02a94e12a17e26
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-04-03 01:35:38 -0600

    add new function and params whit user

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -4,7 +4,19 @@ const Passport = require('passport');
 
 ctrl.profile = async (req, res, next) =>{
     if(req.app.locals.user != undefined){
-        res.send(req.app.locals.user)
+        res.redirect('/profile/'+req.app.locals.user.uniqueId)
+    }else{
+        res.send("null");
+    }
+}
+
+
+ctrl.profileView = async (req, res, next) =>{
+    if(req.app.locals.user != undefined){
+        let viewModel = { title: 'home', layout: 'simplemain'}
+        viewModel.user = req.app.locals.user;
+        console.log("VIEWMODEL USER: "+ viewModel.user)
+        res.render('profile', viewModel)
     }else{
         res.send("null");
     }
