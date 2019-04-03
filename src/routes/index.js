dbab01b06561d1a41a4ae7bad3d6fd88710f3101
Oscar Raygoza <oscar.eduardo.raygoza@gmail.com>
2019-04-02 21:07:12 -0600

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 const home = require('../controllers/home');
+
 const publications = require('../controllers/publications');
 
 module.exports = app =>{
