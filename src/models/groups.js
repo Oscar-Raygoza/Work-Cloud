14b8696c8ece986087a3a94dceaf211391302038
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-04-08 21:22:13 -0500

    change schema [add id_user]

--- src/models/groups.js
+++ src/models/groups.js
@@ -1,12 +1,17 @@
 const { Schema, model } = require("mongoose");
+const ObjectId = Schema.ObjectId;
 
 const GroupSchema = new Schema({
+    user_id: { type: ObjectId },
     name: { type: String },
     description: { type: String },
     image_group: { type: String },
     createAt: { type: Date, default: Date.now()}
 });
 GroupSchema.virtual('uniqueId')
+    .set(function(user){
+        this._user = user;
+    })
     .get(function(){
         return this.image_group.replace(path.extname(this.image_group),'');
     });
