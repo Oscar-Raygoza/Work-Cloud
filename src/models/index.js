8d2b4934c3fbc559aa77be79143b3375de7ab59f
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-04-08 21:22:58 -0500

    export models group and publication_has_group

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,7 @@
 module.exports = {
     Publications: require('./publications'),
     Comment: require('./comments'),
-    User: require('./users')
+    User: require('./users'),
+    Group: require('./groups'),
+    Publication_has_Group: require('./publication_has_group')
 }
