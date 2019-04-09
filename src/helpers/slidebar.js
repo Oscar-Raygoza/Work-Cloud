f57a46f8be7c5ea479486b352a556462459e23f6
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-04-08 23:55:55 -0500

    add groups in slidebar

--- src/helpers/slidebar.js
+++ src/helpers/slidebar.js
@@ -1,18 +1,21 @@
 const Stats = require('./stats');
 const Publications = require('./publications');
 const Comments = require('./comments');
+const Groups = require('./groups');
 
 module.exports = async viewModel=> {
 
     const results = await Promise.all([
         Stats(),
         Publications.popular(),
-        Comments.newest()
+        Comments.newest(),
+        Groups.newGroups()
     ]);
     viewModel.slidebar = {
         stats: results[0],
         popular: results[1],
-        comments: results[2]
+        comments: results[2],
+        groups: results[3]
     }
 
     return viewModel;
