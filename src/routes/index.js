9066c7d47f078bff3e02fe78937ea841eacede77
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-04-08 23:56:57 -0500

    adding function isAuthenticated

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,11 @@ const publications = require('../controllers/publications');
 
 module.exports = app =>{
 router.get('/', isAuthenticated,home.index);
-router.get('/publications/:publication_id', publications.index);
-router.post('/publications/', publications.create);
-router.post('/publications/:publications_id/like', publications.like);
-router.post('/publications/:publications_id/comment', publications.comment);
-router.delete('/publications/:publications_id', publications.delete);
+router.get('/publications/:publication_id', isAuthenticated,publications.index);
+router.post('/publications/', isAuthenticated,publications.create);
+router.post('/publications/:publications_id/like', isAuthenticated,publications.like);
+router.post('/publications/:publications_id/comment', isAuthenticated,publications.comment);
+router.delete('/publications/:publications_id', isAuthenticated,publications.delete);
 router.get('/home', home.home)
 
 function isAuthenticated(req, res, next){
